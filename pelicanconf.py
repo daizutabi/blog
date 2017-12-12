6c33f6afd82e8ec087951d834a438c69746fb546
daizutabi <daizutabi@gmail.com>
2017-12-12 21:00:54 +0900

    Change directory structure

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 AUTHOR = 'daizutabi'
 SITENAME = "Daizutabi's blog"
-SITEURL = ''
-PATH = 'sources'
-OUTPUT_PATH = 'docs/'
+SITEURL = 'https://daizutabi.github.io'
+OUTPUT_PATH = '../daizutabi.github.io/'
 
 TIMEZONE = 'Asia/Tokyo'
 
 DEFAULT_LANG = 'Japanese'
+
+DEFAULT_PAGINATION = 10
+
+RELATIVE_URLS = False
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'daizutabi.github.io/blog'
+SITEURL = 'daizutabi.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
