a0234757e9366767b0e1e60eb35e2ea1a32480ef
daizutabi <daizutabi@gmail.com>
2017-12-13 23:40:31 +0900

    2017/12/13

---
+++ content/2017/12/test.md
@@ -0,0 +1,6 @@
+Title: テストあいう感じd
+Date: 2017-12-12
+Category: Pelican
+Slug: あああああ
+
+テストだよ!
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,28 @@
+RELATIVE_URLS = True
+
+
 AUTHOR = 'daizutabi'
-SITENAME = "Daizutabi's blog"
+SITENAME = '大豆旅'
+# SITESUBTITLE = 'Sub-title that goes underneath site name in jumbotron.'
 SITEURL = 'https://daizutabi.github.io'
 OUTPUT_PATH = '../daizutabi.github.io/'
 
+SLUGIFY_SOURCE = 'title'
+
 TIMEZONE = 'Asia/Tokyo'
+DATE_FORMATS = {
+    'en': '%a, %d %b %Y',
+    'ja': '%Y年%m月%d日 (%a)',
+}
+DEFAULT_LANG = 'ja'
 
-DEFAULT_LANG = 'Japanese'
+DEFAULT_PAGINATION = 5
 
-DEFAULT_PAGINATION = 10
 
-RELATIVE_URLS = False
-
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'page/{slug}/'
+PAGE_SAVE_AS = 'page/{slug}/index.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,6 +30,20 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+THEME = '../voidy-bootstrap/'
+
+# Voidy Bootstrap
+STYLESHEETS = (
+    'voidybootstrap.css',
+    # 'addition/pygment.css',
+    # 'addition/style.css',
+    # 'addition/image.css',
+    # 'addition/admonition.css',
+)
+
+CUSTOM_ARTICLE_HEADERS = ['custom/article_header.jinja2']
+CUSTOM_INDEX_ARTICLE_HEADERS = ['custom/index_article_header.jinja2']
 
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
