f43af9af16851e4385abc7bea057abd349b873a9
daizutabi <daizutabi@gmail.com>
2017-12-17 21:31:39 +0900

    20171217

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,39 +1,33 @@
-RELATIVE_URLS = True
-
-
 AUTHOR = 'daizutabi'
 SITENAME = '大豆旅'
 SITESUBTITLE = 'daizutabiのブログです．'
 SITEURL = 'https://daizutabi.github.io'
 OUTPUT_PATH = '../daizutabi.github.io/'
 
-SLUGIFY_SOURCE = 'title'
-
-TIMEZONE = 'Asia/Tokyo'
-DATE_FORMATS = {
-    'en': '%a, %d %b %Y',
-    'ja': '%Y年%m月%d日 (%a)',
-}
-DEFAULT_LANG = 'ja'
+RELATIVE_URLS = False
 
 DEFAULT_PAGINATION = 5
 
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'ja'
+DATE_FORMATS = {
+    'ja': '%Y年%m月%d日 (%a)',
+}
+
+PATH_METADATA = r'(?P<category>.+?)/.*'
+FILENAME_METADATA = r'(?P<date>\d{8}).(?P<title>.+)'
+# USE_FOLDER_AS_CATEGORY = False
+# FILENAME_METADATA = r'(?P<date>.+?)_(?P<title>.+)'
+# SLUGIFY_SOURCE = 'title'
 
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/index.html'
-PAGE_URL = 'page/{slug}/'
-PAGE_SAVE_AS = 'page/{slug}/index.html'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+PAGE_URL = 'blog/page/{slug}/'
+PAGE_SAVE_AS = 'blog/page/{slug}/index.html'
+CATEGORY_URL = 'blog/category/{name}/'
+CATEGORY_SAVE_AS = 'blog/category/{name}/index.html'
 
 THEME = '../voidy-bootstrap/'
-
-# Voidy Bootstrap
 STYLESHEETS = (
     'custom/custom.css',
     # 'voidybootstrap.css',
@@ -47,6 +41,13 @@ CUSTOM_SITE_HEADERS = ['custom/jumbotron.jinja2']
 CUSTOM_ARTICLE_HEADERS = ['custom/article_header.jinja2']
 CUSTOM_INDEX_ARTICLE_HEADERS = ['custom/index_article_header.jinja2']
 
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
 #          ('Python.org', 'http://python.org/'),
@@ -56,3 +57,6 @@ CUSTOM_INDEX_ARTICLE_HEADERS = ['custom/index_article_header.jinja2']
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
 #           ('Another social link', '#'),)
+
+# PLUGIN_PATHS = ['../pelican-plugins']
+# PLUGINS = []
