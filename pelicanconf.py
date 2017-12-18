c481e8ebcd15464617f4d319f5c7bc71df316653
daizutabi <daizutabi@gmail.com>
2017-12-18 20:30:21 +0900

    Add theme in the repository

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,6 @@ AUTHOR = 'daizutabi'
 SITENAME = '大豆旅'
 SITESUBTITLE = 'daizutabiのブログです．'
 SITEURL = 'https://daizutabi.github.io'
-OUTPUT_PATH = '../daizutabi.github.io/'
 
 RELATIVE_URLS = False
 
@@ -20,14 +19,14 @@ FILENAME_METADATA = r'(?P<date>\d{8}).(?P<title>.+)'
 # FILENAME_METADATA = r'(?P<date>.+?)_(?P<title>.+)'
 # SLUGIFY_SOURCE = 'title'
 
-ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/'
-ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/index.html'
-PAGE_URL = 'blog/page/{slug}/'
-PAGE_SAVE_AS = 'blog/page/{slug}/index.html'
-CATEGORY_URL = 'blog/category/{name}/'
-CATEGORY_SAVE_AS = 'blog/category/{name}/index.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+PAGE_URL = 'page/{slug}/'
+PAGE_SAVE_AS = 'page/{slug}/index.html'
+CATEGORY_URL = 'category/{name}/'
+CATEGORY_SAVE_AS = 'category/{name}/index.html'
 
-THEME = '../voidy-bootstrap/'
+THEME = 'voidy-bootstrap/'
 STYLESHEETS = (
     'custom/custom.css',
     # 'voidybootstrap.css',
