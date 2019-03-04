ab2d7d8f3516278e00a8a248fcfef06821c25a03
daizutabi <daizutabi@gmail.com>
2019-03-04 22:25:02 +0900

    renewal

---
+++ content/Jupyter/20180902_JupyterLabのカスタマイズ.md
@@ -0,0 +1,19 @@
+この記事では，[JupyterLab](https://jupyterlab.readthedocs.io/en/latest/)をカスタマイズする方法を説明します．
+
+
+
+## JupyterLab
+
+JupyterLabを実行する仮想環境を作成し，必要なパッケージをインストールします．
+
+```bash
+(base) conda create -y -n jupyter python=3.6 jupyterlab nodejs
+```
+## jupyterlab-vim
+
+VIMを使えるようにします．
+
+```bash
+(base) > activate jupyter
+(jupyter) > jupyter labextension install jupyterlab_vim
+```
---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+AUTHOR = 'daizutabi'
+SITENAME = '大豆旅'
+SITEURL = 'https://blog.daizutabi.net'
+
+PATH = 'content'
+RELATIVE_URLS = False
+
+DEFAULT_PAGINATION = 10
+
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'Ja'
+DATE_FORMATS = {
+    'ja': '%Y年%m月%d日 (%a)',
+}
+
+PATH_METADATA = r'(?P<category>.+?)/.*'
+FILENAME_METADATA = r'(?P<date>\d{8}).(?P<title>.+)'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+PAGE_URL = 'page/{slug}/'
+PAGE_SAVE_AS = 'page/{slug}/index.html'
+CATEGORY_URL = 'category/{name}/'
+CATEGORY_SAVE_AS = 'category/{name}/index.html'
+
+STATIC_PATHS = ['images', 'extra']
+
+THEME = 'themes/daizu'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+IGNORE_FILES = ['.#*', '__pycache__']
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+FAVICON = 'favicon.ico'
+FAVICON_TYPE = 'image/vnd.microsoft.icon'
+EXTRA_PATH_METADATA = {
+    'extra/' + FAVICON: {'path': FAVICON},
+}
+
+# PLUGINS = ['pheasant']
+# PHEASANT = {'jupyter': None}
